f81186c49190e097df28667befa33f83717847e1
itael <itaelgs@hotmail.com>
2022-12-19 22:37:31 -0600

    agregar ejercicio 2

---
+++ Ejercicio2/Lista_Objeto.js
@@ -0,0 +1,15 @@
+const lista = [
+  "Edy García",
+  31,
+  true,
+  new Date(1991, 1, 04),
+  { 
+      titulo: "Padre Pobre padre rico",
+      autor: "No recuerdo",
+      fecha: new Date(1996, 2, 12),
+      url: "https://www.amazon.es/"
+    },
+];
+
+
+console.log(lista[4].titulo)
