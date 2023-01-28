af46cbe035041fe24115f27d46830cb88e2d95cb
itael <itaelgs@hotmail.com>
2023-01-28 15:54:19 -0600

    Ejercicio 5

---
+++ Ejercicio5/Numbers.js
@@ -0,0 +1,24 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+    const alturaCm = 166
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+    const alturaMt = 1.66
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+    const pesoKg = 78.56
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+    const alturaMtRnd = alturaMt.toFixed(2);
+    console.log(alturaMtRnd)
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+    const pesoKgRnd = Math.round(pesoKg,3);
+    console.log(pesoKgRnd)
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+    // Si max_value_javascript +1 = max_value_javascript?? No entiendo 
+        var numero1 = Number.MAX_NUMBER + 1
+        var numero2 = Number.MAX_NUMBER
+        if (numero1 >= numero2)
+            numero2 
+
+
